5eaf577e308ef7cc97a03275fe395f812c719272
leeheejin <leeheejin@leeheejinui-MacBookPro.local>
2021-03-18 10:04:22 +0900

    .

--- animal-shelter-client/app/src/main/java/com/leeheejin/pms/ClientApp.java
+++ animal-shelter-client/app/src/main/java/com/leeheejin/pms/ClientApp.java
@@ -1,8 +1,70 @@
 package com.leeheejin.pms;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import com.leeheejin.util.Prompt;
+
 public class ClientApp {
 
-  public static void main(String[] args) {
+  String serverAddress;
+  int port;
 
+  public static void main(String[] args) {
+    ClientApp app = new ClientApp("localhost", 8888);
+    app.execute();
+  }
+
+  public ClientApp(String serverAddress, int port) {
+    this.serverAddress = serverAddress;
+    this.port = port;
+  }
+
+  public void execute() {
+    try (Socket socket = new Socket(this.serverAddress, this.port);
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream())) {
+
+      while (true) {
+        String message = Prompt.inputString("명령> ");
+        out.writeUTF(message);
+        out.writeInt(3);
+        out.writeUTF("a");
+        out.writeUTF("b");
+        out.writeUTF("c");
+        out.flush();
+
+        String response = in.readUTF();
+        int length = in.readInt();
+
+        ArrayList<String> data = null;
+        if (length > 0) {
+          data = new ArrayList<>();
+          for (int i = 0; i < length; i++) {
+            data.add(in.readUTF());
+          }
+        }
+
+        System.out.println("------------------------");
+        System.out.printf("작업 결과: %s\n", response);
+        System.out.printf("데이터 개수: %d\n", length);
+        if (data != null) {
+          System.out.println("데이터: ");
+          for (String str : data) {
+            System.out.println(str);
+          }
+        }
+
+
+        if (message.equals("quit")) {
+          break;
+        }
+      }
+      Prompt.close();
+
+    } catch (Exception e) {
+      System.out.println("서버와 통신 하는 중에 오류 발생..");
+    }
   }
 }
--- animal-shelter-server/app/src/main/java/com/leeheejin/pms/ServerApp.java
+++ animal-shelter-server/app/src/main/java/com/leeheejin/pms/ServerApp.java
@@ -1,8 +1,75 @@
 package com.leeheejin.pms;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class ServerApp {
 
-  public static void main(String[] args) {
+  int port;
 
+  public static void main(String[] args) {
+    ServerApp app = new ServerApp(8888);
+    app.service();
+  }
+
+  public ServerApp(int port){
+    this.port = port;
+  }
+
+  public void service() {
+    try (ServerSocket serverSocket = new ServerSocket()){
+
+      System.out.println("서버 실행...");
+
+      processRequest(serverSocket.accept());
+
+    } catch (Exception e) {
+      System.out.println("#서버 실행 중 오류 발생..");
+      e.printStackTrace();
+    }
+  }
+
+  private void processRequest(Socket socket) {
+    try (DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream())){
+
+      while (true) {
+        String request = in.readUTF();
+        int length = in.readInt();
+
+        ArrayList<String> data = null;
+        if (length > 0) {
+          data = new ArrayList<>();
+          for (int i = 0; i <length; i++) {
+            data.add(in.readUTF());
+          }
+        }
+
+        System.out.println("------------------------");
+        System.out.printf("명령: %s\n", request);
+        System.out.printf("데이터 개수: %d\n", length);
+        if (data != null) {
+          System.out.println("데이터: ");
+          for (String str : data) {
+            System.out.println(str);
+          }
+        }
+
+        out.writeUTF("success");
+        out.writeInt(1);
+        out.writeUTF("test..ok");
+        out.flush();
+
+        if (request.equals("quit")) {
+          break;
+        }
+      }
+    } catch (Exception e) {
+      System.out.println("클라이언트의 요청을 처리하는 중 오류 발생..");
+      e.printStackTrace();
+    }
   }
 }
