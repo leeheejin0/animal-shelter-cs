0290e5fdb97d54ef85c12c790f74913ccdce0272
leeheejin0 <jinsis0908@naver.com>
2021-05-03 23:51:40 +0900

    .

--- animal-shelter-client/app/src/main/java/com/leeheejin/pms/dao/ManagerDao.java
+++ animal-shelter-client/app/src/main/java/com/leeheejin/pms/dao/ManagerDao.java
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
-import com.leeheejin.pms.domain.Member;
+import com.leeheejin.pms.domain.Manager;
 
 public class ManagerDao {
 
@@ -17,30 +17,30 @@ public class ManagerDao {
         "jdbc:mysql://localhost:3306/asdb?user=study&password=1111");
   }
 
-  public int insert(Member member) throws Exception {
+  public int insert(Manager manager) throws Exception {
     try (PreparedStatement stmt =
         con.prepareStatement("insert into pms_manager(name,id,password,email,tel,photo) values(?,?,?,?,?,?)");) {
 
-      stmt.setString(1, member.getName());
-      stmt.setString(2, member.getId());
-      stmt.setString(3, member.getPassword());
-      stmt.setString(4, member.getEmail());
-      stmt.setString(5, member.getTel());
-      stmt.setString(6, member.getPhoto());
+      stmt.setString(1, manager.getName());
+      stmt.setString(2, manager.getId());
+      stmt.setString(3, manager.getPassword());
+      stmt.setString(4, manager.getEmail());
+      stmt.setString(5, manager.getTel());
+      stmt.setString(6, manager.getPhoto());
 
       return stmt.executeUpdate();
     } 
   }
 
-  public List<Member> findAll() throws Exception {
-    ArrayList<Member> list = new ArrayList<>();
+  public List<Manager> findAll() throws Exception {
+    ArrayList<Manager> list = new ArrayList<>();
 
     try (PreparedStatement stmt = con.prepareStatement(
         "select no,name,id,email,photo,tel from pms_manager order by name asc");
         ResultSet rs = stmt.executeQuery()) {
 
       while (rs.next()) {
-        Member m = new Member();
+        Manager m = new Manager();
         m.setNo(rs.getInt("no"));
         m.setId(rs.getString("id"));
         m.setName(rs.getString("name"));
@@ -54,7 +54,7 @@ public class ManagerDao {
     return list;
   }
 
-  public Member findByNo(int no) throws Exception {
+  public Manager findByNo(int no) throws Exception {
     try (PreparedStatement stmt = con.prepareStatement(
         "select * from pms_manager where no = ?")) {
 
@@ -65,7 +65,7 @@ public class ManagerDao {
           return null;
         }
 
-        Member m = new Member();
+        Manager m = new Manager();
         m.setNo(no);
         m.setName(rs.getString("name"));
         m.setEmail(rs.getString("email"));
@@ -78,17 +78,17 @@ public class ManagerDao {
     }
   }
 
-  public int update(Member member) throws Exception {
+  public int update(Manager manager) throws Exception {
     try (PreparedStatement stmt = con.prepareStatement(
         "update pms_manager set name=?,id=?,email=?,password=password(?),photo=?,tel=? where no=?")) {
 
-      stmt.setString(1, member.getName());
-      stmt.setString(2, member.getId());
-      stmt.setString(3, member.getEmail());
-      stmt.setString(4, member.getPassword());
-      stmt.setString(5, member.getPhoto());
-      stmt.setString(6, member.getTel());
-      stmt.setInt(7, member.getNo());
+      stmt.setString(1, manager.getName());
+      stmt.setString(2, manager.getId());
+      stmt.setString(3, manager.getEmail());
+      stmt.setString(4, manager.getPassword());
+      stmt.setString(5, manager.getPhoto());
+      stmt.setString(6, manager.getTel());
+      stmt.setInt(7, manager.getNo());
       return stmt.executeUpdate();
     }
   }
@@ -101,7 +101,7 @@ public class ManagerDao {
     }
   }
 
-  public Member findByName(String name) throws Exception {
+  public Manager findByName(String name) throws Exception {
     try (PreparedStatement stmt = con.prepareStatement(
         "select * from pms_manager where name=?")) {
 
@@ -113,7 +113,7 @@ public class ManagerDao {
         return null;
       }
 
-      Member m = new Member();
+      Manager m = new Manager();
       m.setNo(rs.getInt("no"));
       m.setName(rs.getString("name"));
       m.setId(rs.getString("id"));
