dbdf53670c3dd7e46c3d0b4074ef1c80a542a018
leeheejin0 <jinsis0908@naver.com>
2021-05-03 13:51:16 +0900

    .

---
+++ animal-shelter-client/app/src/main/java/com/leeheejin/pms/dao/CatDao.java
@@ -0,0 +1,129 @@
+package com.leeheejin.pms.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import com.leeheejin.pms.domain.Cat;
+import com.leeheejin.pms.domain.Member;
+
+public class CatDao {
+
+  Connection con;
+
+  public CatDao() throws Exception {
+    this.con = DriverManager.getConnection(
+        "jdbc:mysql://localhost:3306/asdb?user=study&password=1111");
+  }
+
+  public int insert(Cat cat) throws Exception {
+    try (PreparedStatement stmt =
+        con.prepareStatement("insert into pms_animal_cat(photo,breed,gender,age,rdt,place,writer) values(?,?,?,?,?,?,?)");) {
+
+      stmt.setString(1, cat.getPhotos());
+      stmt.setString(2, cat.getBreeds());
+      stmt.setString(3, cat.getGenders());
+      stmt.setInt(4, cat.getAges());
+      stmt.setDate(5, cat.getDates());
+      stmt.setString(6, cat.getPlaces());
+
+      return stmt.executeUpdate();
+    } 
+  }
+
+  public List<Cat> findAll() throws Exception {
+    ArrayList<Cat> list = new ArrayList<>();
+
+    try (PreparedStatement stmt = con.prepareStatement(
+        "select id,photo,breed,age,status from pms_animal_cat order by id asc");
+        ResultSet rs = stmt.executeQuery()) {
+
+      while (rs.next()) {
+        Cat c = new Cat();
+        c.setId(rs.getString("id"));
+        c.setPhoto(rs.getString("photo"));
+        c.setEmail(rs.getString("email"));
+        c.setPhoto(rs.getString("photo"));
+        c.setTel(rs.getString("tel"));
+
+        list.add(c);
+      }
+    }
+    return list;
+  }
+
+  public Member findByNo(int no) throws Exception {
+    try (PreparedStatement stmt = con.prepareStatement(
+        "select * from pms_animal_cat where no = ?")) {
+
+      stmt.setInt(1, no);
+
+      try (ResultSet rs = stmt.executeQuery()) {
+        if (!rs.next()) {
+          return null;
+        }
+
+        Member m = new Member();
+        m.setNo(no);
+        m.setName(rs.getString("name"));
+        m.setEmail(rs.getString("email"));
+        m.setPhoto(rs.getString("photo"));
+        m.setTel(rs.getString("tel"));
+        m.setRegisteredDate(rs.getDate("rdt"));
+
+        return m;
+      }
+    }
+  }
+
+  public int update(Member member) throws Exception {
+    try (PreparedStatement stmt = con.prepareStatement(
+        "update pms_animal_cat set name=?,id=?,email=?,password=password(?),photo=?,tel=? where no=?")) {
+
+      stmt.setString(1, member.getName());
+      stmt.setString(2, member.getId());
+      stmt.setString(3, member.getEmail());
+      stmt.setString(4, member.getPassword());
+      stmt.setString(5, member.getPhoto());
+      stmt.setString(6, member.getTel());
+      stmt.setInt(7, member.getNo());
+      return stmt.executeUpdate();
+    }
+  }
+
+  public int delete(int no) throws Exception {
+    try (PreparedStatement stmt = con.prepareStatement(
+        "delete from pms_animal_cat where no=?")) {
+      stmt.setInt(1, no);
+      return stmt.executeUpdate();
+    }
+  }
+
+  public Member findByName(String name) throws Exception {
+    try (PreparedStatement stmt = con.prepareStatement(
+        "select * from pms_animal_cat where name=?")) {
+
+      stmt.setString(1, name);
+
+      ResultSet rs = stmt.executeQuery();
+
+      if (!rs.next()) {
+        return null;
+      }
+
+      Member m = new Member();
+      m.setNo(rs.getInt("no"));
+      m.setName(rs.getString("name"));
+      m.setId(rs.getString("id"));
+      m.setEmail(rs.getString("email"));
+      m.setPhoto(rs.getString("photo"));
+      m.setTel(rs.getString("tel"));
+      m.setRegisteredDate(rs.getDate("rdt"));
+
+      return m;
+    }
+  }
+
+}
